7263197839844ef2b7f16b5f261241bf46959617
lucpimentel <123824068+lucpimentel@users.noreply.github.com>
2023-07-17 22:40:56 -0300

    again

--- app/components/features/hook_evaluation/attention_card.tsx
+++ app/components/features/hook_evaluation/attention_card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Grid, Text, Link } from "@nextui-org/react";
 import EvalLoading from './eval_loading';
 
-const EvaluationResult = ({ apiResponse: any, loading: any }) => {
+const EvaluationResult = ({ apiResponse, loading} : { apiResponse: any; loading: any }) => {
 
   const output = apiResponse && apiResponse.output;
   const rationale = output && JSON.parse(output)["Attention-grabbing hook"]["rationale"];
