f31a8b3341ef9f4624b7d75d107ec1d2651162bf
lucpimentel <123824068+lucpimentel@users.noreply.github.com>
2023-07-19 21:12:25 -0300

    Delete app directory

--- app/components/features/content_repurposer/repurposer_loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Loading } from "@nextui-org/react";
-
-export default function RepurposerLoading() {
-  return <Loading>Loading</Loading>;
-}
--- app/components/features/hook_evaluation/eval_loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Loading } from "@nextui-org/react";
-
-export default function EvalLoading() {
-  return <Loading>Loading</Loading>;
-}
